41a72f51d13d263772dfc8b30d90589900c8cacd
Yiztino <luiselp.080972@gmail.com>
2025-05-30 10:22:29 -0600

    Agregando UI por proximidad
    Ahora, para los objetivos (generadores) la lista de objetivos faltantes también aparece por distancia

--- Assets/Scripts/Managers/ObjectiveManager.cs
+++ Assets/Scripts/Managers/ObjectiveManager.cs
@@ -11,11 +11,37 @@ public class ObjectiveManager : MonoBehaviour
     public GameObject debugTextObject; // Referencia al GameObject que contiene el texto
     private TextMeshProUGUI debugText;
 
+    public Transform playerTransform; // Asignar el transform del jugador
+    public Transform firstObjective;
+
+    private bool hasActivatedProximityText = false;
+
+    private Vector3 firstObjectivePosition;
+
+    public float distanceToActivate;
+
     void Start()
     {
         // Obtener el componente de texto desde el GameObject
         debugText = debugTextObject.GetComponent<TextMeshProUGUI>();
         debugTextObject.SetActive(false); // Asegúrate de que esté oculto al inicio si quieres eso
+        if (firstObjective != null)
+        {
+            firstObjectivePosition = firstObjective.position; // Guardar la posición una vez
+        }
+    }
+    void Update()
+    {
+        if (!hasActivatedProximityText && playerTransform != null)
+        {
+            float distance = Vector3.Distance(playerTransform.position, firstObjectivePosition);
+            if (distance < distanceToActivate)
+            {
+                hasActivatedProximityText = true;
+                debugTextObject.SetActive(true);
+                debugText.text = $"Objetivos destruidos: {destroyedCount}/{totalObjectives}";
+            }
+        }
     }
 
     public void ObjectiveDestroyed()
